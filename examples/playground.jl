8d80f31f4a25688de7941e497e9d8aaf1ab089a4
Emmanuel-R8 <emmanuel.rialland@gmail.com>
2024-01-29 18:16:00 +0800

    Snap

---
+++ examples/playground.jl
@@ -0,0 +1,62 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 164064a0-b8f1-11ee-18e2-a32fba1b90d6
+begin
+	using Pkg
+	Pkg.add(path="..")
+end
+
+# ╔═╡ fa198e49-2bcc-4dfd-b738-176711289de0
+cd("/home/emmanuel/Sync/Development/julia/projects/_repos/CausticsEngineering/examples")
+
+# ╔═╡ 3a78069e-dc1a-48ad-a01e-09a59786fdf9
+
+
+# ╔═╡ d3336e53-d919-41fb-9bc3-5af96906b0eb
+
+
+# ╔═╡ fd929f41-0e85-4464-8e13-596224b08db2
+
+
+# ╔═╡ 18b082f0-7860-48ba-80e2-039bced7f017
+
+
+# ╔═╡ e199ca0f-b7c2-4404-87fb-dcb3880a5075
+
+
+# ╔═╡ eb15f3d3-4f01-4d98-b30c-4a6168755dd5
+
+
+# ╔═╡ 9431d2dc-f6f2-443e-a056-bf1f2f4a464e
+
+
+# ╔═╡ 3d696ab5-5ad9-495a-a4fd-194868060ba1
+
+
+# ╔═╡ 39e25f74-f97b-4b5c-a699-2eb6157eb9b8
+
+
+# ╔═╡ faa84bf8-8ca9-42c0-a723-570e232b0488
+
+
+# ╔═╡ f227507a-c3d6-4cac-957e-998136455d54
+
+
+# ╔═╡ Cell order:
+# ╠═fa198e49-2bcc-4dfd-b738-176711289de0
+# ╠═164064a0-b8f1-11ee-18e2-a32fba1b90d6
+# ╠═3a78069e-dc1a-48ad-a01e-09a59786fdf9
+# ╠═d3336e53-d919-41fb-9bc3-5af96906b0eb
+# ╠═fd929f41-0e85-4464-8e13-596224b08db2
+# ╠═18b082f0-7860-48ba-80e2-039bced7f017
+# ╠═e199ca0f-b7c2-4404-87fb-dcb3880a5075
+# ╠═eb15f3d3-4f01-4d98-b30c-4a6168755dd5
+# ╠═9431d2dc-f6f2-443e-a056-bf1f2f4a464e
+# ╠═3d696ab5-5ad9-495a-a4fd-194868060ba1
+# ╠═39e25f74-f97b-4b5c-a699-2eb6157eb9b8
+# ╠═faa84bf8-8ca9-42c0-a723-570e232b0488
+# ╠═f227507a-c3d6-4cac-957e-998136455d54
